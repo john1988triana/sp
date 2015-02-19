e316040257a5b236cfd7a234980d722bc0c3d5c4
Esteban Romero <esteban@toro-labs.com>
2015-02-19 16:26:25 -0500

    inclusion en  menu codigo promocional

--- application/views/panel_administrativo/header.php
+++ application/views/panel_administrativo/header.php
@@ -87,6 +87,14 @@
 						<li><a href="<?php echo base_url('login/logout'); ?>">Seguimiento</a></li>
 					</ul>
 				</li>
+
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Promociones<span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="<?php echo base_url('login/logout'); ?>">Generar Codigo</a></li>
+						<li><a href="<?php echo base_url('login/logout'); ?>">Ver Codigos</a></li>
+					</ul>
+				</li>
 				
 			</ul>
 		</div><!--/.nav-collapse -->
