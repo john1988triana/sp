810c6b428e5368f89cc1c288168d4ea34aafe634
Esteban Romero <esteban@toro-labs.com>
2015-02-19 15:32:55 -0500

    paneles profesor iguales fix resposive búsqueda

--- application/views/bogota/algebra.php
+++ application/views/bogota/algebra.php
@@ -25,14 +25,13 @@
 
 </div>
 </section>
-<section class="container">
-    <hr id="separador-busqueda">
+<section class="container" id="results-section">
+    <hr id="separador-busqueda" >
 <h2 class="text-center letra-profes">ALGUNOS DE NUESTROS SUPERPROFES DE ALGEBRA</h2>
-	<div class="container">
 		<div class="col-md-12">
-			<div class="col-md-4">
+			<div class="col-md-3">
 				<div class="panel-default panel-perfil">                                            
-				<h3 class="text-center" style="color:#009966;">Miller E. Hernández</h3>
+				<h3 class="text-center tt-txt-panel-perfil" style="color:#009966;">Miller E. Hernández</h3>
 					<div class="panel-body text-center">
 						<img class="img-profile" style="margin: 0px auto;" src="<?php echo base_url("assets/img/profesor-miller-matematicas.png");?>">
 						<div class="estrellas">
@@ -41,17 +40,27 @@
 							<span class="glyphicon glyphicon-star" style="color: #ffcc00;"></span> 
 							<span class="glyphicon glyphicon-star" style="color: #ffcc00;"></span> 
 							<span class="glyphicon glyphicon-star" style="color: #ffcc00;"></span>    
-						</div> 
-						<h4 class="par">Bogotá</h4>  
-						<h4 class="par">Licenciado en Física</h4>    
-						<h4 class="par">Algebra</h4>
-						<h4 class="par">precio: <span style="color: rgb(0, 184, 92);"> $ 35.000 </span></h4>
+						</div>
+						<div class="medallas-profesor">
+							<img src="<?php echo base_url("assets/img/medallas/1_color.png");?>" alt="">
+							<img src="<?php echo base_url("assets/img/medallas/2_color.png");?>" alt="">
+							<img src="<?php echo base_url("assets/img/medallas/3_color.png");?>" alt="">
+							<img src="<?php echo base_url("assets/img/medallas/4_color.png");?>" alt="">
+							<img src="<?php echo base_url("assets/img/medallas/5_bn.png");?>" alt="" class="no-medalla">
+						</div>
+						<h3 class="medalla-tag">Experto</h3>
+
+						<h4 class="par txt-panel-perfil">Bogotá</h4>  
+						<h4 class="par txt-panel-perfil">Licenciado en Física</h4>    
+						<h4 class="par txt-panel-perfil">Algebra</h4>
+						<h4 class="par txt-panel-perfil">precio: <span style="color: rgb(0, 184, 92);"> $ 35.000 </span></h4>
 					</div>
 				</div>
 			</div>
-			<div class="col-md-4">
+
+			<div class="col-md-3">
 				<div class="panel-default panel-perfil">                                            
-					<h3 class="text-center" style="color:#009966;">Carolina Suárez</h3>
+					<h3 class="text-center tt-txt-panel-perfil" style="color:#009966;">Carolina Suárez</h3>
 					<div class="panel-body text-center">
 						<img class="img-profile" style="margin: 0px auto;" src="<?php echo base_url("assets/img/profesora-carolina-matematicas.png");?>">
 						<div class="estrellas">
@@ -68,7 +77,8 @@
 					</div>
 				</div>
 			</div> 
-			<div class="col-md-4">
+
+			<div class="col-md-3">
 				<div class="panel-default panel-perfil">                                            
 					<h3 class="text-center" style="color:#009966;">Daniel Sandoval</h3>
 					<div class="panel-body text-center">
@@ -86,7 +96,27 @@
 						<h4 class="par">Algebra: <span style="color: rgb(0, 184, 92);"> $ 30.000 </span></h4>
 					</div> 
 				</div>  
-			</div> 
+			</div>
+			
+			<div class="col-md-3">
+				<div class="panel-default panel-perfil">                                            
+					<h3 class="text-center" style="color:#009966;">Daniel Sandoval</h3>
+					<div class="panel-body text-center">
+						<img class="img-profile" style="margin: 0px auto;" src="<?php echo base_url("assets/img/profesor-daniel-matematicas.png");?>">
+						<div class="estrellas">
+							<span class="glyphicon glyphicon-star" style="color: #ffcc00;"></span>
+							<span class="glyphicon glyphicon-star" style="color: #ffcc00;"></span> 
+							<span class="glyphicon glyphicon-star" style="color: #ffcc00;"></span> 
+							<span class="glyphicon glyphicon-star" style="color: #ffcc00;"></span> 
+							<span class="glyphicon glyphicon-star" style="color: #ffcc00;"></span>    
+						</div> 
+						<h4 class="par">Bogotá</h4>  
+						<h4 class="par">Estudiante de Economía</h4>    
+						<h4 class="par">Matemáticas</h4>
+						<h4 class="par">Algebra: <span style="color: rgb(0, 184, 92);"> $ 30.000 </span></h4>
+					</div> 
+				</div>  
+			</div>
+
 		</div>  
-	</div>
 </section>
